cfc078b15455069f9deca6d784f74f3e85fda936
Chien Nguyen <chiennn@ants.vn>
2017-08-13 11:02:41 +0700

    update config + add file

--- config/domain/production/default.php
+++ config/domain/production/default.php
@@ -1,6 +1,6 @@
 <?php
-define('SITE_URL', 'chiennn.me');
-define('STATIC_URL', 'static.chiennn.me');
-define('UPLOAD_URL', 'upload.chiennn.me');
-define('FILE_URL', 'files.chiennn.me');
+define('SITE_URL', 'http://chiennn.me');
+define('STATIC_URL', 'http://static.chiennn.me');
+define('UPLOAD_URL', 'http://upload.chiennn.me');
+define('FILE_URL', 'http://files.chiennn.me');
 define('STATIC_VERSION', 1);
