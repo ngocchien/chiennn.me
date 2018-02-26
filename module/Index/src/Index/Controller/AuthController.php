b213ed751dcb9ff8297a2c81122b32086aec090f
chiennn <ngocchien01@gmail.com>
2018-02-26 11:39:52 +0700

    fix layout

--- module/Index/src/Index/Controller/AuthController.php
+++ module/Index/src/Index/Controller/AuthController.php
@@ -10,6 +10,7 @@ namespace Index\Controller;
 
 use MT\Controller\MyController;
 use Google;
+use MT\Nosql;
 
 class AuthController extends MyController
 {
@@ -27,8 +28,70 @@ class AuthController extends MyController
                 'client_id' => '47246308384-09dceuo2a8usraklmtl6mgbfi0q5ljp8.apps.googleusercontent.com',
                 'client_secret' => 'd3NNefkt3ETgbGXdap0hIKLv'
             ];
+            $client = new \Google_Client();
+            $client->setClientId($google_config['client_id']);
+            $client->setClientSecret($google_config['client_secret']);
+            $client->setAccessType("offline");        // offline access
+            $client->setIncludeGrantedScopes(true);   // incremental auth
+            $client->addScope(\Google_Service_Drive::DRIVE_METADATA_READONLY);
+            $client->setRedirectUri('http://dev.chiennn.me/auth/google');
+            $client->setApprovalPrompt("force");
+
+            if(!empty($params['code'])){
+                $client->authenticate($params['code']);
+                $access_token = $client->getAccessToken();
+
+                //write redis info
+                $instanceRedis = Nosql\Redis::getInstance('caching');
+
+                $client->setAccessToken($token);
+
+                $drive = new \Google_Service_Drive($client);
+
+                $files = $drive->files->listFiles();
+
+                //$files = $drive->files->listFiles(array())->getItems();
+                echo '<pre>';
+                print_r($files->getFiles);exit();
+
+                $access_token = $client->getAccessToken();
+                print_r($access_token);die();
+            }
+
+
+
+
+            $client = new \Google_Client();
+            $client->setClientId($google_config['client_id']);
+            $client->setClientSecret($google_config['client_secret']);
+            $client->setAccessType("offline");
+            $client->setApprovalPrompt("force");
+            $client->refreshToken($google_config['refresh_token']);
+            $token = $client->getAccessToken();
+
+            echo '<pre>';
+            print_r($token);
+            echo '</pre>';
+            die();
+
+            $params = array_merge($this->params()->fromRoute(), $this->params()->fromQuery());
+            $google_config = [
+                'client_id' => '47246308384-09dceuo2a8usraklmtl6mgbfi0q5ljp8.apps.googleusercontent.com',
+                'client_secret' => 'd3NNefkt3ETgbGXdap0hIKLv',
+                'refresh_token' => '1/qvdQh4ZAWNGj7McFWxQZ6e_Wh9x84WD8jflAH6Bov8o'
+            ];
+            $client = new \Google_Client();
+            $client->setClientId($google_config['client_id']);
+            $client->setClientSecret($google_config['client_secret']);
+            $client->setAccessType("offline");        // offline access
+            $client->setIncludeGrantedScopes(true);   // incremental auth
+            $client->addScope(\Google_Service_Drive::DRIVE_METADATA_READONLY);
+            $client->setRedirectUri('http://dev.chiennn.me/auth/google');
+            $client->setApprovalPrompt("force");
+
             if(!empty($params['code'])){
 
+//                z-
             }
             $client = new \Google_Client();
             $client->setClientId($google_config['client_id']);
